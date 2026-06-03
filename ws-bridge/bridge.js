ffe3286ae63cff2233cc90ab39eb35d5b4a17e27
Andrew Pampuch <apampuch@protonmail.com>
2026-06-03 15:29:12 +0900

    Partial bridge and admin page implementations

---
+++ ws-bridge/bridge.js
@@ -0,0 +1,26 @@
+const net = require('net');
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 3000 });
+
+wss.on('connection', (ws) => {
+    const relaySocket = net.createConnection('');
+
+    ws.on('message', (data) => {
+        console.log("Received message.");
+        ws.send("Message receieved.");
+
+        // get id
+
+        // extract inner json
+
+        // pass to unix socket
+
+        // send response
+    })
+
+    ws.on('close', (data) => {
+        console.log('Closing.');
+    })
+
+});
