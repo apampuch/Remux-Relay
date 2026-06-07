a5e4af83059f10e8da1ff6f1cb4f37825cd53d08
Andrew Pampuch <apampuch@protonmail.com>
2026-06-07 20:57:20 +0900

    Adds websocket connection for bridge

--- ws-bridge/bridge.js
+++ ws-bridge/bridge.js
@@ -6,19 +6,61 @@ const wss = new WebSocket.Server({ port: 3000 });
 wss.on('connection', (ws) => {
     console.log("Connection established.");
 
-    const relaySocket = net.createConnection('/sockets/relay.sock');
+    function sendCommand(command) {
+        // TODO move this just outside the function for when we make a persistent socket version
+        const relaySocket = net.createConnection('/sockets/relay.sock');
 
-    ws.on('message', (data) => {
-        console.log("Received message.");
-        ws.send("Message receieved.");
+        return new Promise((resolve, reject) => {            
+            relaySocket.once("data", (data) => {
+                resolve(data.toString());
+                relaySocket.end();  // graceful close
+            });
 
-        // get id
+            relaySocket.once("error", (err) => {
+                reject(err);
+                relaySocket.destroy();  // force cleanup
+            });
 
-        // extract inner json
+            relaySocket.write(command);
+        });
+    }
 
-        // pass to unix socket
+    ws.on('message', async (dataJSONString) => {
+        const data = JSON.parse(dataJSONString);
+        const reply = {};
+        const errorObj = {};
 
-        // send response
+        /* check if we have id
+           if we do, add it to the reply */
+        if (Object.hasOwn(data, "id"))
+            reply.id = data.id;
+        else {
+            errorObj.error = "Object has no ID.";
+            ws.send(JSON.stringify(errorObj));
+            return;
+        }
+
+        /* check if we have a command */
+        if (Object.hasOwn(data, "command"))
+            var command = data.command;
+        else {
+            errorObj.error = "Object has no command.";
+            ws.send(JSON.stringify(errorObj));
+            return;
+        }
+
+        // TODO extract other data
+
+        /* just pass the JSON string to the unix socket */
+        reply.message = await sendCommand(dataJSONString); 
+
+        /* wait for response from unix socket */
+
+        console.log("Await finished");
+
+        /* send response */
+        const replyStr = JSON.stringify(reply);
+        ws.send(replyStr);
     })
 
     ws.on('close', (data) => {
