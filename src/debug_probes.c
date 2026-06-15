5cc9cf6ceb9d39fbfda61a03a96f197e932a590b
Andrew Pampuch <apampuch@protonmail.com>
2026-06-15 18:03:03 +0900

    Adds debugging stuff. Mostly commented out at this point.

--- src/debug_probes.c
+++ src/debug_probes.c
@@ -1,35 +1,36 @@
 #include "debug_probes.h"
 
+#include "glib.h"
 #include "gst/gstclock.h"
 #include "gst/gstpad.h"
 #include "gst/gstpipeline.h"
 
-static GstClockTime last_pts = GST_CLOCK_TIME_NONE;
-static GstPadProbeReturn pts_probe(
-    GstPad *pad,
-    GstPadProbeInfo *info,
-    gpointer user_data)
-{
-    if (!(GST_PAD_PROBE_INFO_TYPE(info) & GST_PAD_PROBE_TYPE_BUFFER))
-        return GST_PAD_PROBE_OK;
+// static GstClockTime last_pts = GST_CLOCK_TIME_NONE;
+// static GstPadProbeReturn pts_probe(
+//     GstPad *pad,
+//     GstPadProbeInfo *info,
+//     gpointer user_data)
+// {
+//     if (!(GST_PAD_PROBE_INFO_TYPE(info) & GST_PAD_PROBE_TYPE_BUFFER))
+//         return GST_PAD_PROBE_OK;
 
-    GstBuffer *buf = GST_PAD_PROBE_INFO_BUFFER(info);
-    GstClockTime pts = GST_BUFFER_PTS(buf);
+//     GstBuffer *buf = GST_PAD_PROBE_INFO_BUFFER(info);
+//     GstClockTime pts = GST_BUFFER_PTS(buf);
 
-    if (last_pts != GST_CLOCK_TIME_NONE &&
-        pts < last_pts)
-    {
-        g_print(
-            "PTS WENT BACKWARDS! old=%" GST_TIME_FORMAT
-            " new=%" GST_TIME_FORMAT "\n",
-            GST_TIME_ARGS(last_pts),
-            GST_TIME_ARGS(pts));
-    }
+//     if (last_pts != GST_CLOCK_TIME_NONE &&
+//         pts < last_pts)
+//     {
+//         g_print(
+//             "PTS WENT BACKWARDS! old=%" GST_TIME_FORMAT
+//             " new=%" GST_TIME_FORMAT "\n",
+//             GST_TIME_ARGS(last_pts),
+//             GST_TIME_ARGS(pts));
+//     }
 
-    last_pts = pts;
+//     last_pts = pts;
 
-    return GST_PAD_PROBE_OK;
-}
+//     return GST_PAD_PROBE_OK;
+// }
 
 static void on_consumer_pipeline_created(
     GstElement *sink,
@@ -37,24 +38,46 @@ static void on_consumer_pipeline_created(
     GstPipeline *pipeline,
     gpointer user_data)
 {
-    g_print("Consumer pipeline created: %s\n", consumer_id);
+    static int consumer_count = 0;
+
+    consumer_count++;
+
+    g_print(
+        "Consumer pipeline #%d created: %s\n",
+        consumer_count,
+        consumer_id);
+
+    g_print(
+        "pipeline ptr=%p type=%s\n",
+        pipeline,
+        G_OBJECT_TYPE_NAME(pipeline));
+
+    g_print("Starting recurse\n");
 
     GstIterator *it =
-        gst_bin_iterate_elements(GST_BIN(pipeline));
+        gst_bin_iterate_recurse(GST_BIN(pipeline));
+
+    g_print("Iterator=%p\n", it);
 
     GValue item = G_VALUE_INIT;
 
     while (gst_iterator_next(it, &item) == GST_ITERATOR_OK)
     {
+        g_print("Iterator item\n");
         GstElement *e = GST_ELEMENT(g_value_get_object(&item));
 
         g_print(
-            "  %s (%s)\n",
+            "ELEMENT: %s (%s)\n",
             GST_ELEMENT_NAME(e),
             G_OBJECT_TYPE_NAME(e));
 
         g_value_reset(&item);
     }
+    g_print("Iterator done\n");
+
+    g_print(
+        "Num children: %u\n",
+        GST_BIN_NUMCHILDREN(GST_BIN(pipeline)));
 
     gst_iterator_free(it);
 }
@@ -103,9 +126,14 @@ static GstPadProbeReturn segment_probe(
 }
 
 int add_probes(StreamComponents *components) {
-    // add pad probe 
-    GstPad *pad = gst_element_get_static_pad(components->video_parse, "src");
-    gst_pad_add_probe(pad, GST_PAD_PROBE_TYPE_BUFFER, pts_probe, NULL, NULL);
+    // // add pad probe 
+    // GstPad *pts_pad = gst_element_get_static_pad(components->video_parse, "src");
+    // gst_pad_add_probe(
+    //     pts_pad,
+    //     GST_PAD_PROBE_TYPE_BUFFER,
+    //     pts_probe,
+    //     NULL,
+    //     NULL);
 
     g_signal_connect(
         components->whip_sink,
@@ -120,17 +148,29 @@ int add_probes(StreamComponents *components) {
         TRUE,
         NULL);
 
-    GstPad *segment_pad =
-        gst_element_get_static_pad(
-            components->video_parse,
-            "sink");
+    // GstPad *segment_src_pad =
+    //     gst_element_get_static_pad(
+    //         components->video_parse,
+    //         "src");
 
-    gst_pad_add_probe(
-        segment_pad,
-        GST_PAD_PROBE_TYPE_EVENT_DOWNSTREAM,
-        segment_probe,
-        NULL,
-        NULL);
+    // gst_pad_add_probe(
+    //     segment_src_pad,
+    //     GST_PAD_PROBE_TYPE_EVENT_DOWNSTREAM,
+    //     segment_probe,
+    //     NULL,
+    //     NULL);
+
+    // GstPad *segment_sink_pad =
+    //     gst_element_get_static_pad(
+    //         components->video_parse,
+    //         "sink");
+
+    // gst_pad_add_probe(
+    //     segment_sink_pad,
+    //     GST_PAD_PROBE_TYPE_EVENT_DOWNSTREAM,
+    //     segment_probe,
+    //     NULL,
+    //     NULL);
 
     return 0;
 }
